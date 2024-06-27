5c3dbf0a8866e65b6ffef22ec54fefa98a43bd00
tturdumamatovv <muki2022@mail.ru>
2024-06-27 15:54:39 +0600

    curl

--- apps/medicine/tasks.py
+++ apps/medicine/tasks.py
@@ -1,63 +1,81 @@
 import logging
-import requests
+import subprocess
+import json
 from celery import shared_task
 from django.db import transaction
 from apps.medicine.models import Product
 
+logger = logging.getLogger(__name__)
 
-@shared_task(bind=True)
-def load_products_from_api(self):
-    print("Начало выполнения задачи по загрузке товаров из API")
+@shared_task
+def load_products_from_api():
+    logger.info("Начало выполнения задачи по загрузке товаров из API")
 
+    # Учетные данные для авторизации
     login = "site"
     password = "123"
+
+    # URL для запроса
     url = "http://193.176.239.110/1cBaseCabinet/hs/A8/data"
-    payload = {"Request": "Goods", "Date": "20240313000000"}
+
+    # Тело запроса
+    payload = json.dumps({
+        "Request": "Goods",
+        "Date": "20240313000000"
+    })
 
     try:
-        print("Отправка запроса к API")
-        response = requests.post(url, json=payload, auth=(login, password))
-        response.raise_for_status()  # Добавьте проверку успешного выполнения запроса
-        data = response.json()
+        # Выполнение запроса через curl
+        logger.info("Отправка запроса к API через curl")
+        result = subprocess.run(
+            ["curl", "-u", f"{login}:{password}", "-X", "POST", url, "-d", payload, "-m", "600"],
+            capture_output=True,
+            text=True
+        )
 
-        products_to_create = []
-        products_to_update = []
-        existing_products = {product.code: product for product in Product.objects.all()}
+        if result.returncode == 0:
+            logger.info("Получен успешный ответ от API")
+            data = json.loads(result.stdout)
 
-        for item in data:
-            print(f"Обработка товара: {item['name']} с кодом {item['code']}")
-            if item['code'] in existing_products:
-                product = existing_products[item['code']]
-                product.name = item['name']
-                product.sklad = item['sklad']
-                product.ostatok = item['ostatok']
-                product.price = item['price']
-                product.manufacturer = item['manufacturer']
-                product.country = item['country']
-                products_to_update.append(product)
-            else:
-                products_to_create.append(Product(
-                    code=item['code'],
-                    name=item['name'],
-                    sklad=item['sklad'],
-                    ostatok=item['ostatok'],
-                    price=item['price'],
-                    manufacturer=item['manufacturer'],
-                    country=item['country']
-                ))
+            products_to_create = []
+            products_to_update = []
+            existing_products = {product.code: product for product in Product.objects.all()}
 
-        with transaction.atomic():
-            if products_to_create:
-                Product.objects.bulk_create(products_to_create, batch_size=1000)
-                print(f"Создано товаров: {len(products_to_create)}")
-            if products_to_update:
-                Product.objects.bulk_update(products_to_update, ['name', 'sklad', 'ostatok', 'price', 'manufacturer', 'country'], batch_size=1000)
-                print(f"Обновлено товаров: {len(products_to_update)}")
+            for item in data:
+                logger.debug(f"Обработка товара: {item['name']} с кодом {item['code']}")
+                if item['code'] in existing_products:
+                    product = existing_products[item['code']]
+                    product.name = item['name']
+                    product.sklad = item['sklad']
+                    product.ostatok = item['ostatok']
+                    product.price = item['price']
+                    product.manufacturer = item['manufacturer']
+                    product.country = item['country']
+                    products_to_update.append(product)
+                else:
+                    products_to_create.append(Product(
+                        code=item['code'],
+                        name=item['name'],
+                        sklad=item['sklad'],
+                        ostatok=item['ostatok'],
+                        price=item['price'],
+                        manufacturer=item['manufacturer'],
+                        country=item['country']
+                    ))
 
-        print("Задача по загрузке товаров успешно выполнена")
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка запроса к API: {e}")
-        self.retry(exc=e, countdown=60, max_retries=3)
+            with transaction.atomic():
+                if products_to_create:
+                    Product.objects.bulk_create(products_to_create, batch_size=1000)
+                    logger.info(f"Создано товаров: {len(products_to_create)}")
+                if products_to_update:
+                    Product.objects.bulk_update(products_to_update, ['name', 'sklad', 'ostatok', 'price', 'manufacturer', 'country'], batch_size=1000)
+                    logger.info(f"Обновлено товаров: {len(products_to_update)}")
+
+            logger.info("Задача по загрузке товаров успешно выполнена")
+        else:
+            error_message = f"Ошибка выполнения запроса: {result.stderr}"
+            logger.error(error_message)
+            raise Exception(error_message)
     except Exception as e:
-        print(f"Произошла ошибка: {str(e)}")
-        self.retry(exc=e, countdown=60, max_retries=3)
+        logger.error(f"Произошла ошибка: {str(e)}")
+        raise
